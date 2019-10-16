bc66fb29b80bb3413a94f8d8e00eec7630bc53cf
openbrackettom <tawbrookes@gmail.com>
2019-10-16 23:05:55 +0200

    Cargo Class Structuring
    Added some additional class variables to Cargo & Cargo_Package.
    
    Created various functions for Cargo(s)
    
    Renamed Goods to Cargo_Package.

--- FleetManagementSystemClassLibrary/Cargo.cs
+++ FleetManagementSystemClassLibrary/Cargo.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using MySql.Data.MySqlClient;
+using Dapper;
 namespace FleetManagementSystemClassLibrary
 {
     public class Cargo
@@ -17,19 +18,37 @@ namespace FleetManagementSystemClassLibrary
             get; set;
         }
 
-        public CargoType CargoType
+        public string Cargo_Description
         {
             get; set;
         }
 
-        public List<Goods> Goods
+        public Cargo(int Cargo_ID,decimal Cargo_Weight,string Cargo_Description)
         {
-            get; set;
+            this.Cargo_ID = Cargo_ID;
+            this.Cargo_Weight = Cargo_Weight;
+            this.Cargo_Description = Cargo_Description;
         }
 
         public void packCargo()
         {
             throw new System.NotImplementedException();
+        }
+
+        public void newCargo(decimal Cargo_Weight, string Cargo_Description)
+        {
+            using (MySqlConnection connection = new MySqlConnection(User.LoadConnectionString()))
+            {
+                try
+                {
+                    var output = connection.Query<Cargo>("CALL NewCargo(@Cargo_Weight, @Cargo_Description);", new {Cargo_Weight = Cargo_Weight, Cargo_Description = Cargo_Description }).ToList();
+                    
+                }
+                catch (Exception e)
+                {
+                    
+                }
+            }
         }
 
         public void editCargo()
---
+++ FleetManagementSystemClassLibrary/Cargo_Package.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FleetManagementSystemClassLibrary
+{
+    public class Cargo_Package
+    {
+
+        public string Description
+        {
+            get; set;
+        }
+        public List<Cargo> Cargo_ID
+        {
+            get; set;
+        }
+        public int Trip_ID
+        {
+            get; set;
+        }
+        public decimal Weight
+        {
+            get; set;
+        }
+
+        public void AddCargo(Cargo cargo)
+        {
+            Cargo_ID.Add(cargo);
+        }
+        public Cargo_Package(List<Cargo> Cargo_ID, int Trip_ID, string Description, decimal Weight)
+        {
+            this.Cargo_ID = Cargo_ID;
+            this.Trip_ID = Trip_ID;
+            this.Description = Description;
+            this.Weight = Weight;
+        }
+    }
+}
