c23aef3f0e807f37d2974ffc9a26e3e528b880c2
Thomas Brookes <tawbrookes@gmail.com>
2019-10-22 14:17:37 +0200

    Merges
    Manually added class library changes

--- FleetManagementSystemClassLibrary/Trip.cs
+++ FleetManagementSystemClassLibrary/Trip.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Dapper;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.Collections;
 
 namespace FleetManagementSystemClassLibrary
 {
@@ -9,18 +14,17 @@ namespace FleetManagementSystemClassLibrary
     {
         public int Trip_ID
         {
-            get => default(int);
-            set
-            {
-            }
+
+            get; set;
+            
         }
 
-        public Depot End_Location
+        public string End_Location
         {
             get; set;
         }
 
-        public Depot Start_Location
+        public string Start_Location
         {
             get; set;
         }
@@ -54,25 +58,121 @@ namespace FleetManagementSystemClassLibrary
         {
             get; set;
         }
-
+        public int Driver_ID
+        {
+            get; set;
+        }
         public Vehicle Vehicle
         {
             get; set;
         }
 
-        public List<Cargo> Cargo_Package
+        public Cargo_Package Cargo_Package
         {
             get; set;
         }
 
-        public static void ScheduleTrip()
+        public static void ScheduleTrip(int User_ID,int Vehicle_ID, string Start_Location, string End_Location, int Driver_ID)
+        {
+            using (MySqlConnection connection = new MySqlConnection(User.LoadConnectionString()))
+            {
+                var output = connection.Execute("CALL NewTrip(@User_ID,@Vehicle_ID, @Start_Location, @End_Location, @Driver_ID);", new {User_ID,Vehicle_ID ,Start_Location, End_Location,Driver_ID });
+            }
+        }
+        
+        public static void AddCargo_Package()
+        {
+
+        }
+
+        public static Trip GetTrip(int Trip_ID)
+        {
+            using (MySqlConnection connection = new MySqlConnection(User.LoadConnectionString()))
+            {
+                var output = connection.Query<Trip>("CALL GetTrip(@Trip_ID);", new { Trip_ID }).FirstOrDefault();
+                return output;
+            }
+        }
+        public static List<Trip> getAllTrips()
         {
             throw new System.NotImplementedException();
         }
 
-        public static List<Trip> getAllTrips()
+        public static List<Trip> getAllActiveTrips()
         {
-            throw new System.NotImplementedException();
+            using (MySqlConnection connection = new MySqlConnection(User.LoadConnectionString()))
+            {
+                var output = connection.Query<Trip>("CALL ViewAllActiveTrips();").ToList();
+                return output;
+            }
+        }
+
+        public string GetStartLocation(int Trip_ID)
+        {
+            using (MySqlConnection connection = new MySqlConnection(User.LoadConnectionString()))
+            {
+                var output = connection.Query<string>("CALL GetStartLocation(@Trip_ID);", new {Trip_ID}).ToString();
+                return output;
+            }
+        }
+        public static List<Trip> getAllPendingTrips()
+        {
+            using (MySqlConnection connection = new MySqlConnection(User.LoadConnectionString()))
+            {
+                var output = connection.Query<Trip>("CALL ViewAllPendingTrips();", new DynamicParameters()).ToList(); ;
+                Vehicle a = new Vehicle();
+                IEnumerable<dynamic> iter = connection.Query("CALL ViewAllPendingTrips();", new DynamicParameters()).ToList();
+                int count = 0;
+                foreach (var row in iter)
+                {
+                    Trip temp = output.ElementAt<Trip>(count);
+                    temp.Trip_ID = row.Trip_ID;
+                    
+                    output.ElementAt<Trip>(count).Trip_ID = Convert.ToInt32(row.Trip_ID);
+                    output.ElementAt<Trip>(count).Start_Location = row.Start_Location;
+                    output.ElementAt<Trip>(count).End_Location = row.End_Location;
+                    output.ElementAt<Trip>(count).User = User.GetUser(row.User_ID);
+                    output.ElementAt<Trip>(count).Vehicle = a.getVehicle(Convert.ToInt32(row.Vehicle_ID));
+                    output.ElementAt<Trip>(count).Start_Date = row.Start_Date;
+                    count++;
+                }
+
+                return output;
+            }
+        }
+        public static List<Trip> getAllCompletedTrips()
+        {
+            using (MySqlConnection connection = new MySqlConnection(User.LoadConnectionString()))
+            {
+                var output = connection.Query<Trip>("CALL ViewAllCompletedTrips();", new DynamicParameters()).ToList(); ;
+                Vehicle a = new Vehicle();
+                IEnumerable<dynamic> iter = connection.Query("CALL ViewAllCompletedTrips();", new DynamicParameters()).ToList();
+                int count = 0;
+                foreach (var row in iter)
+                {
+                    Trip temp = output.ElementAt<Trip>(count);
+                    temp.Trip_ID = row.Trip_ID;
+
+                    output.ElementAt<Trip>(count).Trip_ID = row.Trip_ID;
+                    output.ElementAt<Trip>(count).Start_Location = row.Start_Location;
+                    output.ElementAt<Trip>(count).End_Location = row.End_Location;
+                    output.ElementAt<Trip>(count).User = User.GetUser(row.User_ID);
+                    output.ElementAt<Trip>(count).Vehicle = a.getVehicle(Convert.ToInt32(row.Vehicle_ID));
+                    output.ElementAt<Trip>(count).Start_Date = row.Start_Date;
+                    count++;
+                }
+
+                return output;
+            }
+        }
+
+        public static void CompleteTrip(int Trip_ID)
+        {
+           
+            using (MySqlConnection connection = new MySqlConnection(User.LoadConnectionString()))
+            {
+                var output = connection.Execute("CALL CompleteTrip(@Trip_ID);", new {Trip_ID});
+            }
         }
 
         public static Trip getTrip(int Trip_ID)
